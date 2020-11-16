00bc2649bba07136b190ae58c3c834293f62a080
allanhm <heemin.kang@hotmail.com>
2020-11-17 01:08:26 +0900

    second commit

--- characterinfo.h
+++ characterinfo.h
@@ -5,10 +5,23 @@
 #ifndef GROUP_80_CHARACTERINFO_H
 #define GROUP_80_CHARACTERINFO_H
 
-struct Plyaers{
+struct Players{
+    std::string plyaername_v;
+    int plyaerhp_v;
+    int playerlevel_v;
+    int playerattack_v;
+    int plyaerdefense_v;
+    int plyaercard_v[3];
+    int equipment_v[3];
 
 };
-struct Monster{
 
+
+struct Monster{
+    std::string monstername;
+    int Monsterhp_v;
+    int attack_v;
+    int defense_v;
+    int card[3];
 };
 #endif //GROUP_80_CHARACTERINFO_H
