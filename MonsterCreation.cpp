5a001c5f50ff98ff397b1c4c1136c071426ceffc
allanhm <heemin.kang@hotmail.com>
2020-11-18 02:00:21 +0900

    Last Updated at 01:56/ 2020 / 11 /18

--- MonsterCreation.cpp
+++ MonsterCreation.cpp
@@ -7,7 +7,6 @@
 #include <cstdlib>
 #include <ctime>
 void MonsterCreation_f( Monster &enemy, int round){
-    cout << "test";
     std::string monstername_set_v[5] = {"Mike Wazowski","James Sullivan","Pikachu","Kaf","Ian Holiday"};
     srand((unsigned int)time(0));
     int pick = rand()%5;
