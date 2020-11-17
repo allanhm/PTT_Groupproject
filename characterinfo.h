4beea7245e6fccf2b472aeb38109963b372c65ed
allanhm <heemin.kang@hotmail.com>
2020-11-17 19:10:50 +0900

    Last Updated at 19:10/ 2020 / 11 /17

--- characterinfo.h
+++ characterinfo.h
@@ -8,7 +8,6 @@
 struct Players{ //Default value when new characgter is made
     std::string playername_v;
     int playerhp_v = 50;
-    int playerlevel_v = 1;
     int playerattack_v = 7;
     int playerdefense_v = 3;
     int playercard_v[3] = {0};
