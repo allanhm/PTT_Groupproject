111ca23d3c263d998c24383750a3aedf81a0b571
jy0127 <71549092+jy0127@users.noreply.github.com>
2020-11-19 21:42:53 +0900

    Update Battle.cpp

--- Battle.cpp
+++ Battle.cpp
@@ -10,19 +10,20 @@
 #include <algorithm>
 using namespace std;
 
-void CardDraw_f(int usercardhands[], int playerdeck[]);
+void CardDraw_f(int usercardhands[], int playerdeck[]); // declaration of the function
 
-void Redraw_f(int why[], int check[]);
+void Redraw_f(int why[], int check[]); // declaration of the function
 
-void Sort_f(int hands[]);
-void StatusAfterBPlyaer_f(Players user);
+void Sort_f(int hands[]); // declaration of the function
 
-void StatusAFterBMonster_f(Monster enemy);
+void StatusAfterBPlyaer_f(Players user); // declaration of the function
 
-void Battle_f(Players &user, Monster &enemy, int round, int userdeck[], int monsterdeck[]){ // player deack , monster
+void StatusAFterBMonster_f(Monster enemy); // declaration of the function
+
+void Battle_f(Players &user, Monster &enemy, int round, int userdeck[], int monsterdeck[]){ // This function illustrates how the battle is done each round. and decides on who wins or loses.
     int current_weapon = 0, current_armor = 0, item_option;
     vector<int> current_potion;
-    for(int i = 0; i <user.playerequipment_v.size(); i++){
+    for(int i = 0; i <user.playerequipment_v.size(); i++){ // The strength of the items are saved in the text file as 1?? for weapon, 2?? for armor, 3?? for potion. This loop function compares the first digit of the three and updates the strengths of the weapons.
         if(user.playerequipment_v[i]/100 == 1){
             int weapon = user.playerequipment_v[i];
             current_weapon += (weapon%100);
@@ -52,7 +53,7 @@ void Battle_f(Players &user, Monster &enemy, int round, int userdeck[], int mons
     while(keepfight_v) {
         cout <<"-----Item Phase-----" << endl;
         if(current_potion.size() ==0 ){
-            cout<< "There is no potion you can use" <<endl<<endl;
+            cout<< "There is no potion you can use" <<endl<<endl; // shows that there are no potion available 
         }
         else {
             cout << "Current Potion: ";
@@ -61,11 +62,11 @@ void Battle_f(Players &user, Monster &enemy, int round, int userdeck[], int mons
             }
             cout << endl;
             cout << "which one do want to you use?";
-            cin >> item_option;
+            cin >> item_option; // User selects the item
             int option = item_option -1;
             user.playerhp_v += current_potion[option];
-                for(int j =0 ; j < user.playerequipment_v.size();j++){
-                    if (current_potion[option] == user.playerequipment_v[j] - 300 ){
+                for(int j =0 ; j < user.playerequipment_v.size();j++){ // evaluates the different types of items
+                    if (current_potion[option] == user.playerequipment_v[j] - 300 ){ 
                         user.playerequipment_v.erase(user.playerequipment_v.begin() + option);
                         current_potion.erase(current_potion.begin()+option);
                     }
@@ -76,7 +77,7 @@ void Battle_f(Players &user, Monster &enemy, int round, int userdeck[], int mons
 
 
 
-        cout << "-----Attack Phase-----" << endl;
+        cout << "-----Attack Phase-----" << endl; 
         cout << "remaining cards: ";
         for(int i = 0 ; i < 3 ; i++) {
             cout << user.playercard_v[i] << ' ';
@@ -85,12 +86,12 @@ void Battle_f(Players &user, Monster &enemy, int round, int userdeck[], int mons
         cout << user.playername_v << " please select a card to attack: ";
         int userselectedcard_v = 0;
         bool isonhands = false;
-        while (!isonhands) {
+        while (!isonhands) { 
             cin >> userselectedcard_v; //11
             cout << endl;
             for (int i = 0; i < 3; i++) { // take away the user's card
-                if (user.playercard_v[i] == userselectedcard_v) {
-                    user.playercard_v[i] = 0;
+                if (user.playercard_v[i] == userselectedcard_v) { // This if function evaluates whether the user inputted number is equal to one of the numbers in the user's hands.
+                    user.playercard_v[i] = 0; // If it is the same, the card number in the userhands array becomes zero.
                     isonhands = true;
                     break;
                 }
@@ -107,7 +108,7 @@ void Battle_f(Players &user, Monster &enemy, int round, int userdeck[], int mons
         cout << enemy.monstername_v << "'s card is " << monster_card_v << endl;
         cout << endl;
         
-        for (int i = 0; i < 3; i++) { // take away the monster's card
+        for (int i = 0; i < 3; i++) { // take away the monster's card by assigning zero to the monster's hands. Same method as the user.
             if (enemy.monstercard_v[i] == monster_card_v)
                 enemy.monstercard_v[i] = 0;
         }
@@ -116,7 +117,7 @@ void Battle_f(Players &user, Monster &enemy, int round, int userdeck[], int mons
         // --------------------Comparing damage
         if (userselectedcard_v > monster_card_v) { //user wins
             cout << "-----You won!-----" << endl;
-            damage = userselectedcard_v - enemy.monsterdefense_v+current_weapon;
+            damage = userselectedcard_v - enemy.monsterdefense_v+current_weapon; // calculates the damage
             if (damage <= 0) damage =0;
             cout << damage << " damange to the monster!" << endl;
             cout << endl;
@@ -125,15 +126,16 @@ void Battle_f(Players &user, Monster &enemy, int round, int userdeck[], int mons
 
         if (userselectedcard_v <= monster_card_v) { // monster wins
             cout << "-----You lost!-----" << endl;
-            damage = monster_card_v - (user.playerdefense_v+current_armor);
+            damage = monster_card_v - (user.playerdefense_v+current_armor); // calculates the damage
             if (damage <= 0) damage =0;
             cout << damage << " damange to you!" << endl;
             cout << endl;
             user.playerhp_v -= damage;
         }
-        StatusAfterBPlyaer_f(user);
+        
+        StatusAfterBPlyaer_f(user); // Displays the status after the battle
 
-        StatusAFterBMonster_f(enemy);
+        StatusAFterBMonster_f(enemy); // Displays the status after the battle
 
         // if either of the chcarters is down (which is hp < 0) cout << You or Monster loose keepfight => false
         if (user.playerhp_v <= 0) {
@@ -144,19 +146,21 @@ void Battle_f(Players &user, Monster &enemy, int round, int userdeck[], int mons
             cout << "-----You won!-----" << endl;
             keepfight_v = false;
         }
+        
         Sort_f(user.playercard_v); // sort the player's card
         Sort_f(enemy.monstercard_v); // sort the monster's card
-        Redraw_f(user.playercard_v, userdeck);
-        Redraw_f(enemy.monstercard_v, monsterdeck);
+        
+        Redraw_f(user.playercard_v, userdeck); // Redrawing the card to fill in the card that the user has used. 
+        Redraw_f(enemy.monstercard_v, monsterdeck); // Redrawing the card to fill in the card that the monster has used.
 
 
-        // if card num = 0, comapre cout << you win or loose keepfight false ( Card is 0)
+        // if card num = 0, comapre cout << you win or loose keepfight false (Card is 0)
         int numof0_v=0;
         for (int i = 0; i < 20; i++) {
             if (userdeck[i] == 0)
                 numof0_v++;
         }
-        if (numof0_v==20 && user.playerhp_v > enemy.monsterhp_v) { // 모든 댁의 카드가 0이면 numof0_v=20
+        if (numof0_v==20 && user.playerhp_v > enemy.monsterhp_v) { // If all of the cards are assigned as 0(numof0_v==20), that means all the cards in the deck are used. After that process, compare the user and monster HP to decide who wins or loses.
             cout << "-----You won!-----" << endl;
             keepfight_v = false;
         }
@@ -168,15 +172,13 @@ void Battle_f(Players &user, Monster &enemy, int round, int userdeck[], int mons
         }
     }
 
-
-
-void CardDraw_f(int usercardhands[], int playerdeck[]){
+void CardDraw_f(int usercardhands[], int playerdeck[]){ // This function assigns the random card for the user and the monster.
     random_device rd; //random_device for seed value
     minstd_rand gen(rd());
     uniform_int_distribution<int> dis(1,20);
 
     int i = 0, max =3;
-    for (;i < max; i++){
+    for (;i < max; i++){ // Assigns 3 different cards to the user or the monster
         usercardhands[i] =dis(gen);
         for(int j = 0;j<i;j++){
             if (usercardhands[i] == usercardhands[j]){
@@ -187,7 +189,8 @@ void CardDraw_f(int usercardhands[], int playerdeck[]){
         playerdeck[usercardhands[i]] = 0;
     }
 }
-void StatusAfterBPlyaer_f(Players player){
+
+void StatusAfterBPlyaer_f(Players player){ // displays the current status of the user
     cout << "-----User Status-----" << endl;
     cout << "HP: " << player.playerhp_v << " / 50" << endl;
     cout << "Damage: " <<  player.playerattack_v<< endl;
@@ -195,7 +198,7 @@ void StatusAfterBPlyaer_f(Players player){
     cout << endl;
 }
 
-void StatusAFterBMonster_f(Monster mon){
+void StatusAFterBMonster_f(Monster mon){ // displays the current status of the monster
     cout << "-----Monster Status-----" << endl;
     cout << "HP: " << mon.monsterhp_v << " / 50" << endl;
     cout << "Damage: " <<  mon.monsterattack_v << endl;
@@ -213,29 +216,29 @@ void Redraw_f(int why[], int check[]) {
         uniform_int_distribution<int> dis(1, 20);
         int redraw = dis(gen);
         int i = 1;
-        if (check[redraw] == i){
+        if (check[redraw] == i){ // if the randomly generated card is not equal to the card that the user has previously picked, it assigns the card to the user's deck. Same for the monster.
             why[0] = redraw;
             check[redraw] = 0;
             isoverlapped = false;
             }
-        else isoverlapped =true;
+        else isoverlapped =true; // After the card is assigned it exits the loop, otherwise return back to the loop, until a non-repeated card is assigned.
         }
     }
 
 
 
-void Sort_f(int hands[]){
+void Sort_f(int hands[]){ // This function sorts the hands of cards in the ascending order. This is a necessary process for the monster to pick the card. The excludes the lowest number card and randomly picks one the card with the highest and second highest number.
     int i , j, temp;
     for(i=0;i<3;i++)
     {
         for(j=i+1;j<3;j++)
         {
-            if(hands[i]>hands[j])
+            if(hands[i]>hands[j]) // sorts in ascending order
             {
                 temp = hands[i];
                 hands[i]=hands[j];
                 hands[j]=temp;
             }
         }
-    } //sorting in ascending order.
+    } 
 }
