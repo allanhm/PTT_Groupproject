bea083c06a21a69ca74516ebeb344de4bdf85533
jy0127 <71549092+jy0127@users.noreply.github.com>
2020-11-19 21:57:01 +0900

    Update load.cpp

--- load.cpp
+++ load.cpp
@@ -8,21 +8,21 @@ using namespace std;
 void load_f(Players &user) {
     int index =0;
     ifstream fin;
-    string file = user.playername_v + ".txt";
+    string file = user.playername_v + ".txt"; // File according to the user's name.
     fin.open(file); // save hp, attack, defense , card, equipment, round
     
     if ( fin.fail() ) {
-		cout << "Error in file opening!" << endl;
+		cout << "Error in file opening!" << endl; // In case of error in file opening, display that there is a problem.
  		exit(1);
  	}
 
-    cout << "Character name: " << user.playername_v << endl;
+    cout << "Character name: " << user.playername_v << endl; // Check user name in the file.
     fin >> user.playerhp_v;
 
-    fin >> user.playerround_v;
+    fin >> user.playerround_v; // Check the round ended in the file.
     cout << "Current round: " << user.playerround_v << endl;
 
-    cout << "-----Player Status-----" << endl;
+    cout << "-----Player Status-----" << endl; // Check the player status stored  in the file.
     cout << "HP: " << user.playerhp_v << " / 50" << endl;
     fin >> user.playerattack_v;
     cout << "Damage: " <<  user.playerattack_v<< endl;
@@ -31,12 +31,13 @@ void load_f(Players &user) {
 
     cout << "Item info: " << endl;
     int item;
-    while(fin >> item){
+    while(fin >> item){ // If there is an item in the file
         user.playerequipment_v.push_back(item);
     }
+	
     int current_weapon = 0, current_armor = 0, item_option;
     vector<int> current_potion;
-    for(int i = 0; i <user.playerequipment_v.size(); i++){
+    for(int i = 0; i <user.playerequipment_v.size(); i++){ //The strength of the items are saved in the text file as 1?? for weapon, 2?? for armor, 3?? for potion. This loop function compares the first digit of the three and updates the strengths of the weapons.
         if(user.playerequipment_v[i]/100 == 1){
             int weapon = user.playerequipment_v[i]%100;
             current_weapon += (weapon);
@@ -50,6 +51,7 @@ void load_f(Players &user) {
             index++;
         }
     }
+	
     cout << "weapon damage: " << current_weapon << endl;
     cout << "armor defense: " << current_armor << endl;
     cout << "potion: " << index << " bottle(s)"<< endl;
